4f9fa52f2103022841f49a0e2e451acfee8d2322
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-05-25 19:13:34 +0000

    ajuste Console log

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 app.get('/', (resquest, response) => {
-  return response.json({ message: 'Hello World2' });
+  return response.json({ message: 'Hello World' });
 });
 
 app.listen(3333, () => console.log('Server on '));
