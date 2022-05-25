63fbb113cc7e89cf8fe0dd34d834d873ae5cd3a7
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-05-25 12:13:36 +0000

    Criando categoria

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 
 const app = express();
-
+app.use(express.json());
 app.get('/', (resquest, response) => {
-  return response.json({ message: 'Hello World' });
+  return response.json({ message: 'Hello World2' });
 });
 
-app.listen(3333);
+app.listen(3333, () => console.log('Server on '));
