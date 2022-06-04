12b7c8ae29da3a8784c771d593ead74a9231c04a
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-06-03 23:46:18 -0300

    Criando repositório de especificação

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,23 @@
+import { ISpecificationRepository } from '../repositories/ISpecificationRepository';
+
+interface IResquest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log('Executing');
+  constructor(private specificationsRepository: ISpecificationRepository) {}
+
+  execute({ name, description }: IResquest): void {
+    const categoryAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error('Specification already exists!');
+    }
+    this.specificationsRepository.create({ name, description });
+
+    this.specificationsRepository.create({ name, description });
   }
 }
 
