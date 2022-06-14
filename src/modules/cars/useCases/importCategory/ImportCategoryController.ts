160d8dff9d97bec522991d9796db63f0823f8eb0
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-06-14 08:18:06 -0300

    Criando o use case para importar categorias

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { ImportCategoryUseCase } from './importCategoryUseCase';
+
+class ImportCategoryController {
+  constructor(private importCaategoryUsecase: ImportCategoryUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { file } = request;
+    this.importCaategoryUsecase.execute(file);
+    return response.send();
+  }
+}
+
+export { ImportCategoryController };
