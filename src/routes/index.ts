ef458b7ea18f387b79f5ba0c28373d7bd91b9fb2
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-07-12 10:23:02 -0300

    Criando token do usuário

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { authenticateRoutes } from './Authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRouter } from './users.routes';
@@ -8,5 +9,6 @@ const router = Router();
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRouter);
+router.use(authenticateRoutes);
 
 export { router };
