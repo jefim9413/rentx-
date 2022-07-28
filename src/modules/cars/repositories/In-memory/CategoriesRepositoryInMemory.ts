1e670e8b8f414cbd2486779e31dd1e693372b90b
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-07-28 17:12:17 -0300

    Teste de criação de categoria

---
+++ src/modules/cars/repositories/In-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,29 @@
+import { Category } from '../../entities/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from '../ICategoriesRepository';
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  category: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.category.find(c => c.name === name);
+    return category;
+  }
+
+  async list(): Promise<Category[]> {
+    const all = this.category;
+    return all;
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+
+    Object.assign(category, { name, description });
+
+    this.category.push(category);
+  }
+}
+
+export { CategoriesRepositoryInMemory };
